9fad44f2c1bcc57f2574af2394926c10bf419c67
Kenn Torg <kennettorgersenmail@gmail.com>
2023-05-31 12:44:45 +0200

    Pages: Contact and Success

---
+++ emarket/src/pages/ContactPage/ContactSuccess/ContactSuccess.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  padding: 20px;
+  margin: 100px;
+  text-align: center;
+  color: #333;
+
+  h1 {
+    font-size: 36px;
+    font-weight: 500;
+    margin-bottom: 16px;
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 1.5;
+    margin-bottom: 24px;
+  }
+`;
