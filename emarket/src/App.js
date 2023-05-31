c37a73cca4759aa22545c0adc1409c69593aa717
Kenn Torg <kennettorgersenmail@gmail.com>
2023-05-31 12:45:00 +0200

    Updated App and Index

--- emarket/src/App.js
+++ emarket/src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import React from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/ContactPage";
+import Checkout from "./pages/CheckoutPage";
+import CheckoutSuccess from "./pages/CheckoutPage/CheckoutSuccess";
+import ProductList from "./pages/ProductPage/ProductList";
+import CartIcon from "./components/CartIcon";
+//import { GlobalStyles } from "./GlobalStyles";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/checkoutSuccess' element={<CheckoutSuccess />} />
+        <Route path='/product/:id' element={<ProductList />} />
+        <Route path='/cart' element={<CartIcon />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- emarket/src/index.js
+++ emarket/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./pages/Cart/CartContext";
+import { GlobalStyles } from "./GlobalStyles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CartProvider>
+        <GlobalStyles />
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
